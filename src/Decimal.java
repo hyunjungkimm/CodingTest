23089740b741a6cd150e555964e0d460dc3f0af1
kimmmm <dosldnjsss@gmail.com>
2022-01-06 14:42:44 +0900

    프로그래머스 코테 - 소수만들기

---
+++ src/Decimal.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//소수 구하기
+public class Decimal {
+    public static void main(String[]args){
+        //int[]nums = {1,2,3,4};
+        int[]nums = {1,2,7,6,4};
+        int answer= 0;
+        List<Integer> list = new ArrayList<>();
+
+        for(int i =0; i<nums.length; i++){
+            if(i+2 >= nums.length) break;
+            for(int j = i+1; j<nums.length; j++){
+                for(int z = j+1; z<nums.length; z++){
+                    list.add(nums[i]+nums[j]+nums[z]);
+                }
+            }
+        }
+
+        for(int i =0; i<list.size(); i++){
+            int count = 0;
+            for(int j= 2; j<list.get(i); j++){
+                if(list.get(i) % j ==0) count++;
+            }
+            if(count==0){
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
