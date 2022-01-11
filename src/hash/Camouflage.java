afac45c5513e0dc77e287123b089a485a1e0ff29
kimmmm <khjjung1124@gmail.com>
2022-01-11 14:42:09 +0900

    프로그래머스-위장

---
+++ src/hash/Camouflage.java
@@ -0,0 +1,26 @@
+package hash;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Camouflage {
+    public static void main(String[] args) {
+        int answer= 1;
+        String[][] clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String[] clothe: clothes){
+            String key = clothe[1];
+            map.put(key, map.getOrDefault(key, 0)+1);
+        }
+        Iterator<Integer> it = map.values().iterator();
+
+        while(it.hasNext()){
+            answer *= it.next().intValue() +1;
+
+        }
+        System.out.println(answer-1);
+    };
+}
