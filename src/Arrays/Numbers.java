cf731ad9f02ccee2aad3ada4f93bd3a7ee6f11b8
kimmmm <khjjung1124@gmail.com>
2022-01-14 18:32:25 +0900

    프로그래머스 - 가장 큰 수(수정)

--- src/Arrays/Numbers.java
+++ src/Arrays/Numbers.java
@@ -9,21 +9,15 @@ import static java.util.stream.Collectors.toList;
 
 public class Numbers {
     public static void main(String[] args) {
-        int[] numbers = {1, 10, 100, 1000};
-        //int[] numbers ={3,30,34,5,9};
-        String answer = "";
+        //int[] numbers = {1, 10, 100, 1000};
+        int[] numbers ={3,30,34,5,9};
 
-        List<String> list = Arrays.stream(numbers)
-                .mapToObj(String::valueOf)
-                .sorted((s1,s2)->-s1.concat(s2).compareTo(s2.concat(s1)))
-                .collect(toList());
+        String answer = Arrays.stream(numbers)
+                                    .mapToObj(String::valueOf)
+                                    .sorted((a1, a2) -> -a1.concat(a2).compareTo(a2.concat(a1)))
+                                    .reduce("", (a1, a2) ->(a1+a2).equals("0") ?"0" : a1+a2);
 
-
-        for (String i : list) {
-            if(answer.equals("0") && i.equals("0")) return;
-            answer += i;
-        }
-        System.out.println(answer);
+        System.out.println(answer);//9534330
     }
 
 }
