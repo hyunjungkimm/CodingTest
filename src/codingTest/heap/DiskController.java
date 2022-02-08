adc80014a42204377c460ff0224f7d9cf649cace
kimmmm <khjjung1124@gmail.com>
2022-02-08 18:02:15 +0900

    힙(Heap) - 이중순위우선큐

--- src/codingTest/heap/DiskController.java
+++ src/codingTest/heap/DiskController.java
@@ -1,29 +1,36 @@
 package codingTest.heap;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class DiskController {
     public int solution(int[][] jobs) {
         int answer = 0;
-        int answer2 = 0;
+        int now = 0;
+        int i = 0;
+        int count = 0;
 
-        for(int i = 0; i< jobs.length; i++){
-            if(i == 0){
-                answer += jobs[i][1];
-            }else{
-                if(jobs[i-1][1]>jobs[i][0]){
-                    answer += jobs[i][1] - jobs[i][0] +((jobs[i-1][1] - jobs[i][0]));
-                }else{
-                    answer += jobs[i][1];
-                }
+        //배열의 오름차순
+        Arrays.sort(jobs, ((o1,o2)-> o1[0]- o2[0]));
+
+        //우선순위큐 오름차순
+        PriorityQueue<int[]> queue = new PriorityQueue<>(((o1,o2)-> o1[1]-o2[1]));
+
+        while(count < jobs.length){
+            while(i < jobs.length && jobs[i][0] <= now){
+                queue.add(jobs[i++]);
             }
-            System.out.println(answer);
+
+            if(queue.isEmpty()){
+                now = jobs[i][0];
+            }else{
+                int[] temp = queue.poll();
+                answer += temp[1] + now - temp[0];
+                now += temp[1];
+                count++;
+            }
         }
 
         return answer/ jobs.length;
-
     }
 }
--- src/codingTest/heap/Main.java
+++ src/codingTest/heap/Main.java
@@ -4,15 +4,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        /*DiskControlellr
+
         DiskController diskController = new DiskController();
-        int[][] jobs = new int[][]{{0, 3},{1, 9}, {500, 6}};
+        int[][] jobs = new int[][]{{0, 3}, {4, 3}, {8, 3}};
         int answer = diskController.solution(jobs);
         System.out.println(answer);
-         */
+
+        /*PriortyQueue
         String[] operations = new String[]{"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
         PriortyQueue priortyQueue = new PriortyQueue();
         int[] answer = priortyQueue.solution(operations);
         System.out.println(Arrays.toString(answer));
+
+         */
     }
 }
