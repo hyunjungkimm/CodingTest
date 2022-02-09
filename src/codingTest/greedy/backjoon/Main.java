cef55ea74e4b08ebc9f5e986b8c2b92aefc948c7
kimmmm <khjjung1124@gmail.com>
2022-02-09 22:30:03 +0900

    탐욕법(greedy) - 백준 동전0

--- src/codingTest/greedy/backjoon/Main.java
+++ src/codingTest/greedy/backjoon/Main.java
@@ -3,28 +3,28 @@ package codingTest.greedy.backjoon;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class Coin {
-    public int solution(int k) {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         int answer = 0;
+        Scanner scanner = new Scanner(System.in);
 
-        Integer[] coin = {1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000};
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
 
-        Arrays.sort(coin, Collections.reverseOrder());
+        int[] coin = new int[N];
 
-        while(k != 0){
-            for(int i = 0 ; i<coin.length; i++){
-                if(k <= coin[i]){
-                    answer += k/coin[i];
-                    k = k%coin[i];
-                }
+        for(int i = 0; i<N; i++){
+            coin[i] = scanner.nextInt();
+        }
+
+        for(int i = N-1; i>=0; i--){
+            if(coin[i] <= K){
+                answer += K/coin[i];
+                K = K%coin[i];
             }
         }
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        Coin coin = new Coin();
-        int answer = coin.solution(4790);
         System.out.println(answer);
     }
 }
