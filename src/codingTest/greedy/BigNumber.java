9642435c9291e66d2c8bbd8a1b4ab99d50552710
kimmmm <khjjung1124@gmail.com>
2022-02-09 20:36:53 +0900

    탐욕법(greedy) - 더 큰 수 만들기

--- src/codingTest/greedy/BigNumber.java
+++ src/codingTest/greedy/BigNumber.java
@@ -1,38 +1,22 @@
 package codingTest.greedy;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class BigNumber {
     public String solution(String number, int k) {
-        String answer = "";
+        StringBuffer sb = new StringBuffer();
+        int index = 0;
+        int max = 0;
 
-        char[] arr = number.toCharArray();
-        List<Integer> listPre = new ArrayList<>();
-        List<Integer> listAfter = new ArrayList<>();
-        int num = (number.length() - k) / 2;
-        System.out.println(num);
-
-        for(int i = 0; i<arr.length; i++){
-            if(i < arr.length -num){
-                listPre.add(arr[i]-48);
-            }else{
-                listAfter.add(arr[i]-48);
+        for(int i = 0; i<number.length()-k; i++){
+            max = 0;
+            for(int j = index; j<= k+i; j++){
+                if(max < number.charAt(j) - '0'){
+                    max = number.charAt(j) - '0';
+                    index = j+1;
+                }
             }
-        }
-        Collections.sort(listPre, Collections.reverseOrder());
-
-        for(int i = 0; i<num; i++){
-            answer+=listPre.get(i);
-        }
-        if (listPre.get(num) < listAfter.get(0)) {
-            for (int i = 0; i < listAfter.size(); i++) {
-                answer += listAfter.get(i);
-            }
+            sb.append(max);
         }
 
-        return answer;
+        return sb.toString();
     }
 }
