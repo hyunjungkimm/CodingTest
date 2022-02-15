80b828c3a3d0679be9abbe6e97f06730a77150f7
kimmmm <khjjung1124@gmail.com>
2022-02-15 23:57:27 +0900

    프로그래머스 - 다음 큰 수 찾기 완료

--- src/codingTest/practice/NextNumber.java
+++ src/codingTest/practice/NextNumber.java
@@ -7,24 +7,23 @@ public class NextNumber {
         int count = 0;
 
         for(int i =0; i<a.length(); i++){
-            if(a.charAt(i) == '0') count++;
+            if(a.charAt(i) == '1') count++;
         }
 
         while(true){
             int check = 0;
             String b = Integer.toString(n+1, 2);
             for(int i =0; i<b.length(); i++){
-                if(b.charAt(i) == '0') check++;
+                if(b.charAt(i) == '1') check++;
             }
-
-            if(count==check) return Integer.parseInt(b,10);
+            if(count==check) return Integer.parseInt(b,2);
             else n++;
         }
     }
 
     public static void main(String[] args) {
         NextNumber nextNumber = new NextNumber();
-        int answer = nextNumber.solution(78);
+        int answer = nextNumber.solution(15);
         System.out.println(answer);
     }
 }
